8a04ec87e79bd09dcd7e4ee8cadf29d5a54e33a2
VGrokhotov <vlad@grokhotov.ru>
2019-10-07 09:50:06 +0300

    Config added, final edits in lab2

---
+++ lab2/lab2/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace lab2
+{
+    public static class Config
+    {
+        public const uint MAX_ARMY_NUMBER = 6;
+        public const uint MAX_STACK_NUMBER = 999_999;
+    }
+}
--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -8,13 +8,52 @@ namespace lab2
     {
         static void Main()
         {
-            Angel angel1 = new Angel();
-            UnitsStack stack1 = new UnitsStack(angel1, 3);
+            try
+            {
+                Angel angel = new Angel();
+                Devil devil = new Devil();
+                Arbalester arbalester = new Arbalester();
+                Lich lich = new Lich();
 
-            Army gg = new Army(new List<UnitsStack>() {stack1, new UnitsStack(angel1, 8)});
-            var ex = gg.StacksList;
-            ex.Add(new UnitsStack(angel1, 3));
-            Console.WriteLine(gg);
+                UnitsStack stack1 = new UnitsStack(angel, 3);
+                UnitsStack stack2 = new UnitsStack(devil, 3);
+                UnitsStack stack3 = new UnitsStack(arbalester, 3);
+                UnitsStack stack4 = new UnitsStack(lich, 3);
+                List<UnitsStack> unitsStacks = new List<UnitsStack>() {stack1, stack2, stack3, stack4};
+
+                Console.WriteLine("list before editing:");
+                foreach (var stack in unitsStacks)
+                {
+                    Console.WriteLine(stack);
+                }
+                Console.WriteLine("\n\n");
+                
+
+                Army gg = new Army(unitsStacks);
+                Console.WriteLine(gg);
+                Console.WriteLine("\n\n");
+
+                unitsStacks.Add(new UnitsStack(angel, 9));
+                Console.WriteLine("list after editing:");
+                foreach (var stack in unitsStacks)
+                {
+                    Console.WriteLine(stack);
+                }
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("army after editing list:");
+                Console.WriteLine(gg);
+
+                var ex = gg.StacksList;
+                ex.Add(new UnitsStack(angel, 3));
+                Console.WriteLine("army after editing got list:");
+                Console.WriteLine(gg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
