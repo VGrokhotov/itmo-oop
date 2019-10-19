324b8f921116e110e707504f2c7d34b7feee640c
VGrokhotov <vlad@grokhotov.ru>
2019-10-19 18:16:06 +0300

    added lab3

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using  lab2;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
