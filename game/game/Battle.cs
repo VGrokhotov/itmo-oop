f48113be66497fc90d37459d5b63e9634b82647f
VGrokhotov <vlad@grokhotov.ru>
2019-11-16 01:45:58 +0300

    Finished editing of Attack in WaitScale

--- game/game/Battle.cs
+++ game/game/Battle.cs
@@ -73,7 +73,7 @@ namespace game
                     else
                         name = SecondBattleArmy.ArmyName;
                     Console.WriteLine($"Now is turn of: {currentBattleStack.Item1} from Army {name}\n");
-                    Console.WriteLine("Choose one of possible actions:\n[1] Attack\n[2] Defend\n[3] Wiz(Don't choose it)\n[4] Wait(Don't choose it)\n[5] Give up\n");
+                    Console.WriteLine("Choose one of possible actions:\n[1] Attack\n[2] Defend\n[3] Wiz(Don't choose it)\n[4] Wait\n[5] Give up\n");
                     string action = Console.ReadLine();
                     switch (action)
                     {
@@ -123,7 +123,47 @@ namespace game
 
                 while (Scale.WaitScale.Count > 0)
                 {
+                    (BattleUnitsStack, int) currentBattleStack = Scale.WaitScale[0];
+                    Console.WriteLine(Scale);
+                    string name;
+                    if (currentBattleStack.Item2 == 1)
+                        name = FirstBattleArmy.ArmyName;
+                    else
+                        name = SecondBattleArmy.ArmyName;
+                    Console.WriteLine($"Now is turn of: {currentBattleStack.Item1} from Army {name}\n");
+                    Console.WriteLine("Choose one of possible actions:\n[1] Attack\n[2] Defend\n[3] Wiz(Don't choose it)\n[4] Give up\n");
+                    string action = Console.ReadLine();
+                    switch (action)
+                    {
+                        case "1":
+                            Scale.WaitScale.RemoveAt(0);
+                            BattleArmy attackedArmy;
+                            if (currentBattleStack.Item2 == 1)
+                                attackedArmy = SecondBattleArmy;
+                            else
+                                attackedArmy = FirstBattleArmy;
+                            BattleUnitsStack attackedStack = Attacker.attack(currentBattleStack, attackedArmy);
+                            Scale.CheckAttackedStack(attackedStack);
 
+                            break;
+                        case "2":
+                            Scale.WaitScale.RemoveAt(0);
+                            Console.WriteLine("You chose \"Defend\"");
+                            Defend(currentBattleStack.Item1);
+                            break;
+                        case "3":
+                            //Scale.WaitScale.RemoveAt(0);
+                            break;
+                        case "4":
+                            //Scale.WaitScale.RemoveAt(0);
+                            Console.WriteLine("You chose \"Give up\"");
+                            GiveUp(currentBattleStack);
+                            break;
+                        default:
+                            Console.WriteLine("Incorrect command, try again");
+                            break;
+
+                    }
 
                     if (HasBattleEnded)
                     {
